a9aad18adad491cea62de177eb1456b4743b2d19
Chayanon01 <chayanon.tree@mail.kmutt.ac.th>
2017-04-01 01:35:43 +0700

    Add files via upload

--- TimeImage.java
+++ TimeImage.java
@@ -1,42 +1,47 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.awt.Color;
+
 /**
  * Write a description of class TimeImage here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class TimeImage extends KeyAndButton
-{
+public class TimeImage extends KeyAndButton {
+
     /**
-     * Act - do whatever the TimeImage wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - do whatever the TimeImage wants to do. This method is called
+     * whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
-    int time=180;
+
+    int time = 180;
     int count = 0;
-    public TimeImage(){
+
+    public TimeImage() {
         time = 180;
-        GreenfootImage gg = new GreenfootImage(""+time,15,Color.BLACK,null);
+        GreenfootImage gg = new GreenfootImage("" + time, 15, Color.BLACK, null);
         setImage(gg);
-        
+
     }
-    public void act() 
-    {
+
+    public void act() {
         count++;
-        
-        if(count == 60){
+
+        if (count == 60) {
             time--;
             updateTime();
-            count=0;
+            count = 0;
         }
-    }  
-    public void updateTime(){
-        
-        GreenfootImage gg = new GreenfootImage(""+time,15,Color.WHITE,null);
+    }
+
+    public void updateTime() {
+
+        GreenfootImage gg = new GreenfootImage("" + time, 15, Color.WHITE, null);
         setImage(gg);
     }
-    public int getTime(){
+
+    public int getTime() {
         return time;
     }
 }
